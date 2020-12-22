e458d268a8b103c49c31b5c5ad90dd940b5778af
Scott Joyner <47367649+scottjoyner@users.noreply.github.com>
2020-12-22 16:03:55 -0500

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -1,4 +1,62 @@
 import useSWR from 'swr'
+import React, { useState } from 'react';
+import {
+  Collapse,
+  Navbar,
+  NavbarToggler,
+  NavbarBrand,
+  Nav,
+  NavItem,
+  NavLink,
+  UncontrolledDropdown,
+  DropdownToggle,
+  DropdownMenu,
+  DropdownItem,
+  NavbarText
+} from 'reactstrap';
+
+const navbar = (props) => {
+  const [isOpen, setIsOpen] = useState(false);
+
+  const toggle = () => setIsOpen(!isOpen);
+
+  return (
+    <div>
+      <Navbar color="dark" dark expand="md">
+        <NavbarBrand href="/">Kipnerter</NavbarBrand>
+        <NavbarToggler onClick={toggle} />
+        <Collapse isOpen={isOpen} navbar>
+          <Nav className="mr-auto" navbar>
+            <NavItem>
+              <NavLink href="/">Components</NavLink>
+            </NavItem>
+            <NavItem>
+              <NavLink href="https://github.com/scottjoyner">GitHub</NavLink>
+            </NavItem>
+            <UncontrolledDropdown nav inNavbar>
+              <DropdownToggle nav caret>
+                Links
+              </DropdownToggle>
+              <DropdownMenu right>
+                <DropdownItem>
+                  Option
+                </DropdownItem>
+                <DropdownItem>
+                  Option 2
+                </DropdownItem>
+                <DropdownItem divider />
+                <DropdownItem>
+                  Reset
+                </DropdownItem>
+              </DropdownMenu>
+            </UncontrolledDropdown>
+          </Nav>
+          <NavbarText>Simple Text</NavbarText>
+        </Collapse>
+      </Navbar>
+    </div>
+  );
+}
 
 const fetcher = (query) =>
   fetch('/api/graphql', {
@@ -20,10 +78,13 @@ export default function Index() {
   const { users } = data
 
   return (
+    <>
+    <navbar/>
     <div>
       {users.map((user, i) => (
         <div key={i}>{user.name}</div>
       ))}
     </div>
+    </>
   )
 }
